02a6a8922a0324a434b569a14afc49c304f86ca8
Hayden Sim <haydensim@hotmail.com>
2019-08-23 19:39:54 +1000

    added: more useless fiddling with main.go, nothing serious yet

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/briandowns/spinner"
 	"github.com/hysmio/gcloud-interactive/gcloud"
-	"github.com/hysmio/gcloud-interactive/gcloud/kubernetes"
 	"github.com/manifoldco/promptui"
 )
 
@@ -15,10 +15,10 @@ func main() {
 	// Declare spinner - use https://github.com/briandowns/spinner to determine charset
 	spin := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 	infoText := promptui.Styler(promptui.FGBold)
-	extraText := promptui.Styler(promptui.FGFaint, promptui.FGBold)
+	// extraText := promptui.Styler(promptui.FGFaint, promptui.FGBold)
 	errorText := promptui.Styler(promptui.FGRed, promptui.FGBold)
 	successText := promptui.Styler(promptui.FGGreen, promptui.FGBold)
-	attrText := promptui.Styler(promptui.FGMagenta, promptui.FGBold)
+	// attrText := promptui.Styler(promptui.FGMagenta, promptui.FGBold)
 
 	// Verify gcloud is installed
 	spin.Prefix = "Verifying gcloud & kubectl are installed "
@@ -63,36 +63,72 @@ func main() {
 
 	// TODO: Asyncronously read in more configuration details from gcloud
 
-	spin.Prefix = "Getting clusters "
+	// spin.Prefix = "Getting clusters "
+	// spin.Start()
+	// clusters, err := kubernetes.GetClusters()
+	// if err != nil {
+	// 	spin.Stop()
+	// 	fmt.Printf("Couldn't get clusters: %s", err)
+	// 	os.Exit(1)
+	// }
+	// spin.Stop()
+
+	// cluster := kubernetes.Cluster{}
+
+	// if len(clusters) <= 0 {
+
+	// } else if len(clusters) == 1 {
+	// 	cluster = clusters[0]
+	// } else {
+	// 	prompt := promptui.Select{
+	// 		Label: "Select a Cluster",
+	// 		Items: clusters,
+	// 	}
+
+	// 	prompt.Run()
+	// }
+
+	// fmt.Printf("%s %s %s\n", infoText("Selected Cluster: "), attrText(cluster.Name), extraText("("+cluster.MasterIP+")"))
+
+	spin.Prefix = infoText("Getting projects ")
 	spin.Start()
-	clusters, err := kubernetes.GetClusters()
-	if err != nil {
-		spin.Stop()
-		fmt.Printf("Couldn't get clusters: %s", err)
-		os.Exit(1)
-	}
+	projects, _ := gcloud.GetAllProjects()
 	spin.Stop()
 
-	cluster := kubernetes.Cluster{}
+	project := gcloud.Project{}
 
-	if len(clusters) <= 0 {
+	if len(projects) == 0 {
+		// TODO: Add ability to create projects
+		println(errorText("You have no projects, exiting..."))
+		os.Exit(1)
+	} else if len(projects) == 1 {
+		project = projects[0]
+		println(infoText("Found 1 project!"))
+		activeProject, _ := gcloud.GetActiveProject()
 
-	} else if len(clusters) == 1 {
-		cluster = clusters[0]
-	} else {
-		prompt := promptui.Select{
-			Label: "Select a Cluster",
-			Items: clusters,
+		println(activeProject)
+
+		if strings.TrimSpace(activeProject) != project.ID {
+			println(errorText("We only found one project, but it is not your currently active project"))
+			prompt := promptui.Prompt{
+				Label: infoText("Would you like to set ") +
+					project.Format() +
+					infoText(" as your current project? (Y/n) "),
+			}
+
+			result, _ := prompt.Run()
+
+			if result != "n" {
+				spin.Prefix = infoText("Setting current project to " + project.Format())
+				spin.Start()
+				gcloud.SetActiveProject(project.ID)
+				spin.Stop()
+				println(successText("Set ") + project.Format() + successText(" as active project"))
+			}
 		}
-
-		prompt.Run()
+	} else {
+		// TODO: Get current project, let user choose between projects
 	}
-
-	fmt.Printf("%s %s %s\n", infoText("Selected Cluster: "), attrText(cluster.Name), extraText("("+cluster.MasterIP+")"))
-
-	projects, err := gcloud.GetAllProjects()
-
-	fmt.Printf("Found project: %s %s\n", attrText(projects[0].Name), extraText("("+projects[0].ID+")"))
 
 	// // Get the current project
 	// spin.Prefix = "Getting current GCloud project: "
