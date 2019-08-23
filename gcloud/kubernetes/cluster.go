3e603255eb8b7e50b9ccc532f3a4384f14b1412f
Hayden Sim <haydensim@hotmail.com>
2019-08-23 19:39:23 +1000

    refactor: refactored clusters to use the table parser method

--- gcloud/kubernetes/cluster.go
+++ gcloud/kubernetes/cluster.go
@@ -1,7 +1,7 @@
 package kubernetes
 
 import (
-	"strings"
+	"github.com/hysmio/gcloud-interactive/parser"
 
 	"github.com/hysmio/gcloud-interactive/gcloud"
 )
@@ -28,38 +28,23 @@ func CreateCluster(name string) error {
 func GetClusters() ([]Cluster, error) {
 	raw, err := gcloud.CloudCommand("container", "clusters", "list").Output()
 	output := string(raw)
-	// output := "NAME              LOCATION                     MASTER_VERSION             MASTER_IP           MACHINE_TYPE          NODE_VERSION         NUM_NODES         STATUS\n"
-	// output += "automated-cloud   australia-south-east-a       some-kubernetes-version    192.168.213.123     A v smol one          node version         2                 RUNNING"
 
 	clusters := make([]Cluster, 0)
+	rows := parser.ParseTable(output)
 
-	location := strings.Index(output, "LOCATION")
-	masterVersion := strings.Index(output, "MASTER_VERSION")
-	masterIP := strings.Index(output, "MASTER_IP")
-	machineType := strings.Index(output, "MACHINE_TYPE")
-	nodeVersion := strings.Index(output, "NODE_VERSION")
-	numNodes := strings.Index(output, "NUM_NODES")
-	status := strings.Index(output, "STATUS")
-
-	for pos, value := range strings.Split(output, "\n") {
-		if pos == 0 {
-			continue
+	for _, r := range rows {
+		cluster := Cluster{
+			Name:          r["NAME"],
+			Location:      r["LOCATION"],
+			MasterVersion: r["MASTER_VERSION"],
+			MasterIP:      r["MASTER_IP"],
+			MachineType:   r["MACHINE_TYPE"],
+			NodeVersion:   r["NODE_VERSION"],
+			NumNodes:      r["NUM_NODES"],
+			Status:        r["STATUS"],
 		}
 
-		if len(value) > status {
-			cluster := Cluster{
-				Name:          strings.TrimSpace(value[0:location]),
-				Location:      strings.TrimSpace(value[location:masterVersion]),
-				MasterVersion: strings.TrimSpace(value[masterVersion:masterIP]),
-				MasterIP:      strings.TrimSpace(value[masterIP:machineType]),
-				MachineType:   strings.TrimSpace(value[machineType:nodeVersion]),
-				NodeVersion:   strings.TrimSpace(value[nodeVersion:numNodes]),
-				NumNodes:      strings.TrimSpace(value[numNodes:status]),
-				Status:        strings.TrimSpace(value[status:len(value)]),
-			}
-
-			clusters = append(clusters, cluster)
-		}
+		clusters = append(clusters, cluster)
 	}
 
 	return clusters, err
